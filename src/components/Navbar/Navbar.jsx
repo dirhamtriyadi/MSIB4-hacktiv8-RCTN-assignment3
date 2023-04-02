cbbc5878d87879fe53614fc2fdf52ee5107229f7
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-04-02 12:24:06 +0700

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Dirham from '../../assets/dirham.jpg'
 
-
 import './Navbar.css'
 
 const Navbar = () => {
@@ -10,13 +9,13 @@ const Navbar = () => {
     <nav>
         <div className='navbar'>
           <img src={Dirham} alt='Dirham' className='rounded-circle mb-2' height='150px' />
-                <Link className='mb-2' to={'/'}>Home</Link>
-                <Link className='mb-2' to={'/about'}>About</Link>
-                <Link className='mb-2' to={'/experience'}>Experience</Link>
-                <Link className='mb-2' to={'/education'}>Education</Link>
-                <Link className='mb-2' to={'/skills'}>Skills</Link>
-                <Link className='mb-2' to={'/interest'}>Interest</Link>
-                <Link className='mb-2' to={'/awards'}>Awards</Link>
+            <Link className='mb-2' to={'/'}>Home</Link>
+            <Link className='mb-2' to={'/about'}>About</Link>
+            <Link className='mb-2' to={'/experience'}>Experience</Link>
+            <Link className='mb-2' to={'/education'}>Education</Link>
+            <Link className='mb-2' to={'/skills'}>Skills</Link>
+            <Link className='mb-2' to={'/interest'}>Interest</Link>
+            <Link className='mb-2' to={'/awards'}>Awards</Link>
         </div>
     </nav>
   )
